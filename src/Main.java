17e16b91a2ff0b0f9651e2cdc048b2ccd795fefb
onyekalice <onyekaa@missionreadyhq.com>
2026-05-26 12:43:20 +1200

    Initial commit - add Main.java with version 1.0 output

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// BIT504 - Assessment 2, Task 9
+// Onyeka Alice ARAPAI (5129321)
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("======== BIT504 - Assessment 2, Task 9 ========");
+		System.out.println("Demonstrating source and version control.");
+		System.out.println("Version 1.0");
+
+	}
+
+}
